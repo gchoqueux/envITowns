79b80d53ce4a745112fb22f82bf43f729b7309cd
gchoqueux <gerald.choqueux@ign.fr>
2020-01-22 11:14:08 +0100

    creer env de travail

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const webpack = require("webpack");
+const path = require("path");
+
+let config = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "./public"),
+        filename: "./bundle.js"
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, "./public"),
+        historyApiFallback: true,
+        inline: true,
+        open: true,
+        hot: true
+    },
+    devtool: "eval-source-map"
+}
+
+module.exports = config;
