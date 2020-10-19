b9a57a41e7040e53bf7671bcacc284f527812edb
gchoqueux <gerald.choqueux@ign.fr>
2020-10-19 09:54:57 +0200

    Blank project empty itowns

---
+++ src/code.md
@@ -0,0 +1,9 @@
+* creer une vue avec une origine
+* ajout d'une couche wmts
+* evenement View
+* evenement Controls
+* FrameRequester
+* Place et orienter un objet 3d sur le globe
+* Ajouter une couche raster avec source vectorielle
+* Ajouter une couche raster avec source vectorielle avec ajout d'un nouveau parser 
+*  
--- src/index.js
+++ src/index.js
@@ -1,32 +1,20 @@
 import * as itowns from 'itowns';
 
 const placement = {
-    coord: new itowns.Coordinates('EPSG:4326', 2.35, 48.85),
-    range: 250000,
+    coord: new itowns.Coordinates('EPSG:4326', 2.42, 48.83),
+    range: 2500000,
 }
 
 const viewerDiv = document.getElementById('viewerDiv');
 const view = new itowns.GlobeView(viewerDiv, placement);
 const layer = new itowns.ColorLayer('mon calque', {
-	source: new itowns.WMTSSource({
-		'url': 'https://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/wmts',
-        'projection': 'EPSG:3857',
-        'name': 'ORTHOIMAGERY.ORTHOPHOTOS',
-        'tileMatrixSet': 'PM',
-        'format': 'image/jpeg',
-	})
+    source: new itowns.WMTSSource({
+        url: 'https://wxs.ign.fr/3ht7xcw6f7nciopo16etuqp2/geoportail/wmts',
+        projection: 'EPSG:3857',
+        name: 'ORTHOIMAGERY.ORTHOPHOTOS',
+        tileMatrixSet: 'PM',
+        format: 'image/jpeg',
+    })
 });
 
 view.addLayer(layer);
-
-// http://www.itowns-project.org/itowns/docs/#api/View/View
-view.addFrameRequester(itowns.MAIN_LOOP_EVENTS.AFTER_CAMERA_UPDATE, () => {
-	console.log('after update camera');
-});
-
-
-// http://www.itowns-project.org/itowns/docs/#api/Controls/GlobeControls
-view.controls.addEventListener(itowns.CONTROL_EVENTS.CAMERA_TARGET_CHANGED, () => {
-	console.log('CAMERA_TARGET_CHANGED');
-});
-
